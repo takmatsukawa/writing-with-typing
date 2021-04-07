f791e5a77025fe9ae6bd157d9abec5b99799daee
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-04-07 22:38:42 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-4xl">test</h1>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style></style>
