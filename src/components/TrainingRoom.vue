3b67981f46326c85346e6f73dfce65e6b0346cf2
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-04-10 22:19:56 +0900

    Auto resize textarea

--- src/components/TrainingRoom.vue
+++ src/components/TrainingRoom.vue
@@ -8,7 +8,14 @@
       <answer-text :answer="textSet.target" :input="inputs[i]"></answer-text>
       <p class="text-gray-800 mt-5">{{ textSet.native }}</p>
       <textarea
+        :ref="
+          (el) => {
+            // @ts-ignore
+            if (el) textareas[i] = el;
+          }
+        "
         v-model="inputs[i]"
+        :style="{ height: heights[i] }"
         class="mt-2 px-2 py-1 shadow-sm block w-full border border-gray-300 rounded-md focus:outline-none focus:ring sm:text-sm"
       ></textarea>
     </section>
@@ -16,8 +23,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from "vue";
-import { useRouter } from "vue-router";
+import {
+  defineComponent,
+  ref,
+  computed,
+  nextTick,
+} from "vue";
 import { useTextSets } from "../composables/useTextSets";
 import AnswerText from "../components/AnswerText.vue";
 
@@ -44,11 +55,36 @@ export default defineComponent({
       pack.value ? pack.value.textSets.map(() => "") : []
     );
 
+    const textareas = ref<Element[]>([]);
+    const heights = ref<string[]>(
+      pack.value ? pack.value.textSets.map(() => "auto") : []
+    );
+
+    const resize = () => {
+      textareas.value.forEach((textarea, i) => {
+        heights.value[i] = "auto";
+        nextTick(() => {
+          heights.value[i] = textarea.scrollHeight + "px";
+        });
+      });
+    };
+
     return {
       title: pack.value?.title,
       texts: pack.value?.textSets,
       inputs,
+      heights,
+      textareas,
+      resize,
     };
+  },
+  watch: {
+    inputs: {
+      handler(val) {
+        this.resize();
+      },
+      deep: true,
+    },
   },
   components: {
     AnswerText,
