9ae1b7a73479bf07c4fae1e75d7f7d337039e854
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-04-12 01:28:08 +0900

    タンタン

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -11,17 +11,17 @@
         <span class="md:text-xl"> Writing Training for English Learner </span>
       </span>
     </header>
-    <main
-      class="max-w-prose mx-5 md:mx-auto flex mt-16 flex-col space-y-2 md:flex-row md:space-x-4 md:space-y-0"
-    >
+    <main class="max-w-prose mx-5 mt-16 md:mx-auto">
       <p v-if="!packages.length" class="text-gray-800">There is no package!</p>
-      <router-link
-        v-for="pack in packages"
-        :key="pack.id"
-        :to="{ name: 'training-room', params: { id: pack.id } }"
-        class="border block px-8 py-5 rounded hover:border-gray-300"
-        >{{ pack.title }} ({{ pack.textSets.length }})</router-link
-      >
+      <div class="grid gap-1 md:grid-cols-3 md:gap-4">
+        <router-link
+          v-for="pack in packages"
+          :key="pack.id"
+          :to="{ name: 'training-room', params: { id: pack.id } }"
+          class="border inline-block px-8 py-5 rounded hover:border-gray-300"
+          >{{ pack.title }} ({{ pack.textSets.length }})</router-link
+        >
+      </div>
     </main>
   </div>
 </template>
