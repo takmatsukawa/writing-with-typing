4d5582517adcd0d25a9dbc89921bdf53e2b8c00a
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-04-11 22:46:23 +0900

    デザイン調整

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="pb-10">
     <header class="bg-gray-50 border-b">
-      <div
-        class="flex items-center bg-gradient-to-r from-green-600 to-green-500 text-white px-8 py-4"
+      <span
+        class="flex items-center bg-gradient-to-r from-green-600 to-green-500 text-white px-8 py-4 md:inline-flex"
       >
         <alphabetical-variant-icon
           class="inline-block mr-2 transform -skew-y-6"
           decorative
         />
         <span class="md:text-xl"> Writing Training for English Learner </span>
-      </div>
+      </span>
     </header>
     <main
       class="max-w-prose mx-5 md:mx-auto flex mt-16 flex-col space-y-2 md:flex-row md:space-x-4 md:space-y-0"
