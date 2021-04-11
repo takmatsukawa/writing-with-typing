812b61509058076c8ab8ffe7ee5d1d1fb11fe00a
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-04-11 13:03:26 +0900

    テキストはYAMLで管理できるようにした

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,9 +1,10 @@
 <template>
   <main>
-    <h1 class="text-4xl">Writing training for English learner</h1>
+    <h1 class="text-4xl">Writing Training for English Learner</h1>
     <div
       class="flex mt-8 flex-col space-y-2 md:flex-row md:space-x-4 md:space-y-0"
     >
+      <p v-if="!packages.length" class="text-gray-800">There is no package!</p>
       <router-link
         v-for="pack in packages"
         :key="pack.id"
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import yaml from "@rollup/plugin-yaml";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/writing-with-typing/",
-  plugins: [vue()],
+  plugins: [vue(), yaml()],
 });
